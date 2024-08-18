ec8e3844a992e50684dc5648a02970056616022c
Ben Kochie <superq@gmail.com>
2024-08-18 06:13:10 +0200

    Update grpc library use (#6826)
    Replace deprecated grpc Dial/DialContext with grpc.NewClient.
    
    Signed-off-by: SuperQ <superq@gmail.com>

--- test/grpc_test.go
+++ test/grpc_test.go
@@ -3,7 +3,6 @@ package test
 import (
 	"context"
 	"testing"
-	"time"
 
 	"github.com/coredns/coredns/pb"
 
@@ -23,9 +22,7 @@ func TestGrpc(t *testing.T) {
 	}
 	defer g.Stop()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	conn, err := grpc.DialContext(ctx, tcp, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+	conn, err := grpc.NewClient(tcp, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
 	if err != nil {
 		t.Fatalf("Expected no error but got: %s", err)
 	}
