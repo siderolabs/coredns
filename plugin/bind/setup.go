23e9004f63ae975e7ba605cfba71c0bc391a9d02
Tobias Klauser <tobias.klauser@gmail.com>
2024-10-24 16:24:08 +0200

    Replace k8s.io/utils/strings/slices by Go stdlib slices (#6863)
    Use the functionality provided by the Go standard library slices package
    since Go 1.21 instead of the deprecated functions in the
    k8s.io/utils/strings/slices package.
    
    Signed-off-by: Tobias Klauser <tobias@isovalent.com>

--- plugin/bind/setup.go
+++ plugin/bind/setup.go
@@ -4,13 +4,12 @@ import (
 	"errors"
 	"fmt"
 	"net"
+	"slices"
 
 	"github.com/coredns/caddy"
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
 	"github.com/coredns/coredns/plugin/pkg/log"
-
-	"k8s.io/utils/strings/slices"
 )
 
 func setup(c *caddy.Controller) error {
