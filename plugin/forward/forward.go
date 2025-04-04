e16162dd3ca12bc5cb5abb2c0beff1dcd03fe231
Parfenov Ivan <74457414+Paramoshka@users.noreply.github.com>
2025-04-04 23:27:47 +0700

    Added `SetProxyOptions` function for `forward` plugin (#7229)
    Signed-off-by: Paramoshka <parfenov_ivan_42a@mail.ru>

--- plugin/forward/forward.go
+++ plugin/forward/forward.go
@@ -75,6 +75,11 @@ func (f *Forward) SetProxy(p *proxy.Proxy) {
 	p.Start(f.hcInterval)
 }
 
+// SetProxyOptions setup proxy options
+func (f *Forward) SetProxyOptions(opts proxy.Options) {
+	f.opts = opts
+}
+
 // SetTapPlugin appends one or more dnstap plugins to the tap plugin list.
 func (f *Forward) SetTapPlugin(tapPlugin *dnstap.Dnstap) {
 	f.tapPlugins = append(f.tapPlugins, tapPlugin)
